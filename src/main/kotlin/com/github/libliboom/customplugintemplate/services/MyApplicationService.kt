640811ca3a8351c1f23fca3c43a73533d7bb8e0d
GitHub Action <action@github.com>
2022-03-05 01:23:26 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/libliboom/customplugintemplate/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.libliboom.customplugintemplate.services
+
+import com.github.libliboom.customplugintemplate.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
